b010fa1333ee203184847eabdb2bc59da17c6556
akazadev <codemma3@gmail.com>
2024-12-25 10:43:11 +0000

    second commit

--- app/components/header.js
+++ app/components/header.js
@@ -2,16 +2,16 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="w-full font-semibold flex flex-row justify-between items-center">
-      <div className="flex-1 font-black">
+    <div className="w-full font-semibold flex flex-col md:flex-row justify-between items-center">
+      <div className="flex-1 font-black text-center md:text-left">
         <p>TheCode</p>
       </div>
-      <div className="flex-1 flex justify-center space-x-14">
+      <div className="flex-1 flex justify-center space-x-4 md:space-x-14 mt-4 md:mt-0">
         <p>GitHub</p>
         <p>LinkedIn</p>
         <p>Twitter</p>
       </div>
-      <div className="flex-1 flex justify-end space-x-14">
+      <div className="flex-1 flex justify-center md:justify-end space-x-4 md:space-x-14 mt-4 md:mt-0">
         <p>MyPortfolio</p>
         <p>MyResume</p>
       </div>
--- app/components/maincard.js
+++ app/components/maincard.js
@@ -6,20 +6,21 @@ import Image from 'next/image';
 
 const Maincard = () => {
     return (
-        <div className={`relative px-10 py-8 pb-40 bg-[#FCF7F4] w-11/12 h-[680px] flex flex-col items-center rounded-xl gap-8 overflow-hidden`}>
+        <div className={`relative px-4 py-8 pb-20 md:px-10 md:pb-40 bg-[#FCF7F4] w-full md:w-11/12 h-auto md:h-[680px] flex flex-col items-center rounded-xl gap-8 overflow-hidden`}>
             <Header />
             
             <div className='w-full flex flex-col justify-center items-center leading-none'>
-                <h1 className="uppercase leading-none font-anton text-[135px] tracking-wider">merry christmas</h1>
-                <p className="text-[#121212] jolly-lodger-regular text-6xl leading-none">To you all❤</p>
+                <h1 className="uppercase leading-none font-anton text-4xl md:text-[135px] tracking-wider">merry christmas</h1>
+                <p className="text-[#121212] jolly-lodger-regular text-xl md:text-6xl leading-none">To you all❤</p>
             </div>
 
             <div className='w-full flex flex-col justify-center items-center leading-none'>
                 <Image 
                     src="/santa.png" 
                     alt="Santa" 
-                    width={460} 
-                    height={500} 
+                    width={230} 
+                    height={250} 
+                    className="md:w-[460px] md:h-[500px]"
                 />
             </div>
         </div>
